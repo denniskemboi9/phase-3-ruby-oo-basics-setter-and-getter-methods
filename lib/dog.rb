6ddeef1da2efaa596a51d05e58d967100629cc75
DennisKemboi <cherogonydennis9@gmail.com>
2023-05-19 23:02:39 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def name= (dog_name)
+        @name = dog_name
+    end
 
+    def name
+        @name
+    end
+
+    def breed= (new_breed)
+        @breed = new_breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+dog1 = Dog.new
+puts dog1.name = "Rex"
+puts dog1.breed = "GS"
